6b9ce82196cf0b1aeefbe82a1fed0b92a2c3b44e
Spider <rushabh6792@gmail.com>
2016-09-23 17:56:14 +0530

    Enabling Google Authentication

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
 (function(){
-    
     // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAR7qvA_ndwO9dQWTrRIT_aSOGxHljiSKE",
-    authDomain: "mychatapp-1bc8b.firebaseapp.com",
-    databaseURL: "https://mychatapp-1bc8b.firebaseio.com",
-    storageBucket: "mychatapp-1bc8b.appspot.com",
-    messagingSenderId: "902956545483"
-  };
-  firebase.initializeApp(config);
+      var config = {
+        apiKey: "AIzaSyCI2DbiO8zooI0HxiXVRrFYhwx8lgZc2JI",
+        authDomain: "gflex-chat-5bf2b.firebaseapp.com",
+        databaseURL: "https://gflex-chat-5bf2b.firebaseio.com",
+        storageBucket: "gflex-chat-5bf2b.appspot.com",
+        messagingSenderId: "416462513454"
+      };
+      firebase.initializeApp(config);
     
-    //Fetching the DOM elements.
-    const inputName = document.getElementById("inputText");
-    const addButtonRef = document.getElementById("addButton");
-    const ulListRef = document.getElementById("viewList");
+    //Database reference
+    const dbRef = firebase.database().ref().child("Chats");
+    const messageRef = dbRef.child("Messages");
     
-    //Creating a database reference
-    const dbRef = firebase.database().ref().child("users");
-    const studentRef = dbRef.child("students");
+    //Login Authentication variables
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
     
-    //Fetching the list
-    studentRef.on('child_added', snap => {
-            
-            const li = document.createElement("li");
-            li.id = snap.key;
-            li.innerText = snap.val();
-            ulListRef.appendChild(li);
-    });
-    studentRef.on('child_removed', snap => {
-        const li = document.getElementById(snap.key);
-        li.remove();
-    });
+    //Refer to ID's of all buttons and elements in index.html
+    const loginRef = document.getElementById("loginBtn");
     
-    addButtonRef.addEventListener('click', function(snapshot){
-        var getName = inputName.value;
-        studentRef.push(getName);
-    });
-    
-    
-    
+    loginRef.addEventListener('click', function(){
+        
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                  // This gives you a Google Access Token. You can use it to access the Google API.
+                  var token = result.credential.accessToken;
+                  // The signed-in user info.
+                  var user = result.user;
+                console.log(user);
+                  // ...
+            }).catch(function(error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  // The email of the user's account used.
+                  var email = error.email;
+                  // The firebase.auth.AuthCredential type that was used.
+                  var credential = error.credential;
+                console.log(error);
+                  // ...
+            });
+    })
 }());
